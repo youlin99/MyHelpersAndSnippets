94d5111006e5ba43e8737be6871c60f02ecb0e23
hongyl <hongyl@outlook.com>
2018-09-03 17:02:01 +0800

    init setup

---
+++ MyHelpersAndSnippets/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHelpersAndSnippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
